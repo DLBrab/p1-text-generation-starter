64871f8791cad8de140641eebd618bb8e5937ece
DLBrab <82184836+DLBrab@users.noreply.github.com>
2022-06-21 23:17:36 -0700

    Assignment finished

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # p1-text-generation-starter
 
 Credit: Adam Smith
+
+Add a level of complexity. The example uses two ways of expansion: replacing a general word with one of several specific ones; replacing a single word with multiple ones of the same type: (adventurer --> Jack, Jill, and Bob). Add one additional type of replacement. In your README, specify what it is. (3 pts)
+    The set of text I wrote takes much more advantage of CFGs and how they can be used, permitting for items to be replaced by storylets to keep details consistent. For example, a Scallywag cant tell you to walk the plank no matter how much you want them to.
+
+appropriate title and accurate description for your project (2 pts)
+    Space or Pirates; A phrase about either space-related content or pirate-related content
+artist statement: What is the effect you're trying to achieve? Did you achieve that effect? If not, what was the difficulty/limitation with tools, and what did you end up doing? (This last bit might be more relevant with later, more complex assignments.) (2 pts)
+    The idea was to express two separate storylets that keep details consistent. But it ended up turning into an exceedingly high amount of writing to potentially do more than just a sentence of generation with the structure I was working by. The idea was to have it create some sort of humorous statement or strong opinion on which Mass Effect game is the best. As stated, the main issue with this, and the added level of complexity I made, was that the amount of writing I would need to do would be monumental, and is likely the reason why more condensed forms of generative writing now exist.
--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const fillers = {
-    adventurer: ["My dude", "Bro", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-    pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-    post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca"],
-    item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-    baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-    message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
-    
+    story:          ["$pirates", "$space"],
+    pirates:        ["$pGreeting I'm $pName, $pStoryType"],
+    space:          ["$sGreeting$sProtagStory"],
+
+    pGreeting:      ["Ahoy!", "Avast, ye harties!", "Yo ho ho ho!"],
+    pName:          ["Brook", "Fiddlesticks", "Tartar Sauce", "Cap'n Knuckles", "Mr. Krabs"],
+    pStoryType:     ["Captain of the $pShipName, and I $pCapEnding", "Scallywag of the $pShipName, and I $pScallEnding", "First Mate of the $pShipName, and I $pFMEnding"],
+    pShipName:      ["S.S. Britannica", "Going Merry", "Thousand Sunny", "Poopdeck", "Bartlebelly"],
+    pCapEnding:     ["sentence ye to walk the plank!", "demand ye fork over all ye cash!", "ain't never heard of ye", "need oranges, please give me oranges or me and me mates'll have skurrrvay! :("],
+    pScallEnding:   ["am going to walk the plank soon. :(", "just wanna afford another Funko Pop...", "know why they call it the Poopdeck and I aint tellin ye."],
+    pFMEnding:      ["actually am loyal and don't exist as a plot vehicle to create a conflict for the protagonist captain to learn his folly.", "AM TOTALLY GONNA MUTINY THIS SHIP LIKE EVERY OTHER FIRST MATE!!!"],
+
+    sGreeting:      ["Hello ", "Good day ", "How are you ", ""],
+    sProtagStory:   ["Commander Shepard, I see you've been killing plenty of $MEStoryMeme", "Doomguy, it seems we have committed the folly of trifling with demonic energy and $DoomMeme", "Gentlemen? All your base are belong to us. $AYBABTU"],
+    AYBABTU:        ["You are on the way to destruction, make your time. Ha ha ha ha. $GetZiggyWithIt"],
+    GetZiggyWithIt: ["For great justice, take off every zig.$MOVEZIG"],
+    MOVEZIG:        [" Move, Zig, Move, Zig, Move, Zig, You know what you're doing? take off every zig.$MOVEZIG", "", "", "", ""],
+    MEStoryMeme:    ["Reapers. Too bad theres 3 recolors of the same ending.", "Geth. I hope you're giving them a damn good time.", "Collectors. You're the best one around."],
+    DoomMeme:       ["oops we spilled some of it and now theres demons, can you rip and tear them all up?"],
   };
   
-  const template = `$adventurer, heed my $message!
-  
-  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
-  
-  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
-  `;
+  const template = `$story`;
   
   
   // STUDENTS: You don't need to edit code below this line.
